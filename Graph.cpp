95750660fab7e863339080960427c2c11b3db811
FaKeArtur27 <102204205+FaKeArtur27@users.noreply.github.com>
2023-06-02 19:17:12 -0600

    Add files via upload

---
+++ Graph.cpp
@@ -0,0 +1,303 @@
+#include "Graph.h"
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include "Incidencia.h"
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <vector>
+
+
+Graph::Graph() {
+  adjList.clear();
+}
+
+Graph::Graph(std::ifstream &input) {
+  std::string mes;
+  std::string dia;
+  std::string horas;
+  std::string ip_origen;
+  std::string ip_destino;
+  std::string peso;
+  std::string razon;
+  std::string error1;
+  std::string error2;
+  std::string error3;
+  std::string error4;
+  std::string error5;
+  std::string error6;
+
+  
+  std::vector<Incidencia> nodosOrigen;
+  std::vector<Incidencia> incidencias;
+  
+  std::string line;
+  // std::map<int, std::string> ipInfo; //Map 
+  
+  int i = 0;
+
+  while (std::getline(input, line)) {
+    if (i == 0) {
+      std::vector<int> result;
+      split(line, result);
+      numNodes = result[0];
+      numEdges = result[1];
+
+      // Reservar memoria para renglones de la lista de adyacencia
+      // Nodos son uno basados (renglon 0 no sera usado)
+      adjList.resize(numNodes + 1);
+      // Declarar un lista vacia para cada renglon y se almacena en el vector
+      for (int k = 1; k <= numNodes; k++) {
+        LinkedList<std::pair<std::string, int>> tmpList;
+        adjList[k] = tmpList;
+      }
+    }
+      
+    //Guardar nodos
+    else if (i <= numNodes){
+      Incidencia nodoOrigen;
+      nodoOrigen.setIpValue(line, 0);
+      nodosOrigen.push_back(nodoOrigen);
+    }
+      
+    // Guardar incidencias
+    else if(i <= numNodes + numEdges){
+      std::istringstream ss(line);
+      // Obtenemos los datos que estan separados por espacios
+      ss >> mes >> dia >> horas >> ip_origen >> ip_destino >> peso >> error1 >> error2 >> error3 >> error4 >>
+          error5 >> error6;
+      
+      //std::cout<< ip_origen << "----" << ip_destino <<std::endl;
+      Incidencia IncidenciaTemp(mes,dia,horas,ip_origen,ip_destino, peso, razon);
+      IncidenciaTemp.cambiarFormato(ip_origen, ip_destino, peso);
+      incidencias.push_back(IncidenciaTemp);
+    }
+    i++;
+  }
+
+  // Ordenar nodos
+  std::sort(nodosOrigen.begin(), nodosOrigen.end());
+
+   // Crear mapa para almacenar nodos y su numero de orden
+  std::map<int, std::string> nodosOrdenados;
+  for (int j = 0; j < (int)nodosOrigen.size(); j++) {
+    nodosOrdenados[j + 1] = nodosOrigen[j].getIpOrigen();
+  }
+  
+  // Construir lista de adyacencia - -- - -- -- - 
+  std::sort(incidencias.begin(), incidencias.end());
+  
+  int x=0;
+  int numV = 0;
+  std::string nombre = nodosOrigen[0].getIpOrigen();
+  
+  for(int i = 1; i <= numEdges; i++) {
+    std::string nombre_temp = nombre;
+    nombre = incidencias[i-1].getIpOrigen();
+    
+    if(nombre_temp != nombre){
+      nodosOrigen[x].setNumVecinos(numV);
+      x++;
+      numV = 0;
+    }
+      
+    int nodoU = x;
+    
+    std::string nodoV = incidencias[i-1].getIpDestino();
+    int weight = incidencias[i-1].getPeso();
+
+    // grafo dirigido agregar aristas (u,v) unicamente 
+    adjList[nodoU].addLast(std::make_pair(nodoV, weight));
+    numV++;
+  }
+
+  std::ofstream outputFile("grados_ips.txt");
+  if(outputFile.is_open()){
+    for(int i = 1; i <= numNodes; i++){
+      outputFile <<"(" << nodosOrigen[i].getIpOrigen() << "," << nodosOrigen[i].getNumVecinos() << ")" << std::endl;
+    }
+    outputFile.close();
+  }
+  else{
+    std::cout << "Error con el archivo." << std::endl;
+  }
+
+  // std::cout << nodosOrigen[13356].getNumVecinos() << std::endl;
+  // std::cout << nodosOrigen[13357].getNumVecinos() << std::endl;
+  // std::cout << nodosOrigen[13358].getNumVecinos() << std::endl;
+  // std::cout << nodosOrigen[13359].getNumVecinos() << std::endl;
+  // std::cout << nodosOrigen[13369].getNumVecinos() << std::endl;
+}
+
+Graph::~Graph() {
+  adjList.clear();
+}
+
+int Graph::getNumEdges(){
+  return numEdges;
+}
+
+std::vector<LinkedList<std::pair<std::string, int>>> Graph::getAdjList(){
+  return adjList;
+}
+
+
+void Graph::split(std::string line, std::vector<int> & res) {
+    size_t strPos = line.find(" ");
+    size_t lastPos = 0;
+    while (strPos != std::string::npos) {
+      res.push_back(stoi(line.substr(lastPos, strPos - lastPos)));
+      lastPos = strPos + 1;
+      strPos = line.find(" ", lastPos);
+    }
+    res.push_back(stoi(line.substr(lastPos, line.size() - lastPos)));
+}
+/*
+void Graph::loadDirWeightedGraph(std::istream & input) {
+  std::string line;
+  int i = 0;
+  while (std::getline(input, line)) {
+    if (i == 0) {
+      i++;
+      continue;
+    }
+    if (i == 1) {
+      std::vector<int> result;
+      split(line, result);
+      numNodes = result[0];
+      numEdges = result[2];
+      // Reservar memoria para renglones de la lista de adyacencia
+      // Nodos son uno basados (renglon 0 no sera usado)
+      adjList.resize(numNodes + 1);
+      // Declarar un lista vacia para cada renglon y se almacena en el vector
+      for (int k = 1; k <= numNodes; k++) {
+        LinkedList<std::pair<std::string, int>> tmpList;
+        adjList[k] = tmpList;
+      }
+      i++;
+      continue;
+    }
+    // Lectura de aristas del grafo
+    std::vector<int> result;
+    split(line, result);
+    int nodoU = result[0];
+    int nodoV = result[1];
+    int weight = result[2];
+    // grafo dirigido agregar aristas (u,v) unicamente 
+    adjList[nodoU].addLast(std::make_pair(nodoV, weight));
+    i++;
+  }
+  
+}
+*/
+
+void Graph::print() {
+  std::cout << "numNode: " << numNodes << std::endl;
+  std::cout << "numEdges: " << numEdges << std::endl;
+  std::cout << "Adjacency List" << std::endl;
+  for (int nodeU = 1; nodeU <= numNodes; nodeU++) {
+    std::cout << "vertex " << nodeU << ": ";
+    NodeLinkedList<std::pair<std::string, int>> *ptr = adjList[nodeU].getHead();
+    while (ptr != nullptr ) {
+      std::pair<std::string, int> par = ptr->data;
+      std::cout << "{" << par.first << "," << par.second << "} ";
+      ptr = ptr->next;
+    }
+    std::cout << std::endl;
+  }
+}
+
+
+// void Graph::BFS(int v) {
+//   // Declarar un set del STL de C++ (elementos unicos y ordenados)
+//   std::set<int> visited;
+//   // Crear un queue
+//   QueueLinkedList<int> queue;
+//   // Marca el vertice de partida v como visitado y lo mete en el queue
+//   visited.insert(v);
+//   queue.enqueue(v);
+//   std::cout << "Recorrido BFS " << std::endl;
+//   while (!queue.isEmpty()) {
+//     // Extraemos un vertice de el queue
+//     v = queue.getFront();
+//     queue.dequeue();
+//     std::cout << v << " ";
+//     // Obtener los vecinos del vertice v
+//     // Si estos no han sido visitados marcarlos como visitados
+//     // y los metemos a el queue
+//     NodeLinkedList<std::pair<int, int>> *ptr = adjList[v].getHead();
+//     while (ptr != nullptr) {
+//       std::pair<int, int> par = ptr->data;
+//       int u = par.first; // nodo u es vecino de v
+//       // Verificar si el vertice u (vecino de v) ya fue visitado
+//       bool isVisited = visited.find(u) != visited.end();
+//       if (!isVisited) { // no visitado
+//         visited.insert(u); // marcar como visitado
+//         queue.enqueue(u); // meter en el queue
+//       }
+//       ptr = ptr->next;
+//     }
+//   }
+//   std::cout << std::endl;
+// }
+
+
+// Complejidad O((|V | + |E|) log |V |)
+
+// void Graph::dijkstraAlgorithm(int v) {
+//   // Crear una priority queue del STL de C++
+//   // https://www.geeksforgeeks.org/implement-min-heap-using-stl/
+//   std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> pq;
+//   // vector de distancias con el resultado del algoritmo
+//   std::vector<int> dist(numNodes+1, INF);
+//   // Insertar el nodo de origen v en la priority queue -- pares (dist, vertice)
+//   pq.push(std::make_pair(0, v)); // la distancia de v a v es cero
+//   dist[v] = 0;
+//   // Mientras el priority queue no este vacio
+//   while (!pq.empty()) {
+//     // Extraer un vertice de el priority queue
+//     int nodeU = pq.top().second; // pares (dist, vertice)
+//     pq.pop();
+//     // Obtener los vecions del vertice nodeU
+//     NodeLinkedList<std::pair<std::string, int>> *ptr = adjList[nodeU].getHead();
+//     while (ptr != nullptr) {
+//       std::pair<std::string, int> par = ptr->data;
+//       std::string nodeV = par.first; // nodeV es vecino de nodeU
+//       int peso = par.second; // peso de la arista (nodoU,nodeV)
+//       if (dist[nodeV] > dist[nodeU]+peso) {
+//         // Actualizar la distancia (mas corta) de nodeV
+//         dist[nodeV] = dist[nodeU] + peso;
+//         pq.push(std::make_pair(dist[nodeV], nodeV));
+//       }
+//       ptr = ptr->next;
+//     }
+//   }
+
+// std::ofstream outputFile("distancia_bootmaster.txt");
+//   if(outputFile.is_open()){
+//     for(int i = 1; i <= numNodes; i++){
+//       if(dist[i] != INF)
+//         outputFile << i << " " << dist[i] << std::endl;
+//       else
+//         outputFile << i << " INF" << std::endl;
+//     }
+//     outputFile.close();
+//   }
+//   else{
+//     std::cout << "Error con el archivo." << std::endl;
+//   }
+
+
+//   //if(outputFile)
+  
+//   // Imprimir las distancias mas cortas entre v y todos los nodos del grafo
+//   std::cout << "Vertice\tDistancia desde v" << std::endl;
+//   for (int i = 1; i <= numNodes; i++) {
+//     if (dist[i] != INF)
+//       std::cout << i << "\t" << dist[i] << std::endl;
+//     else
+//       std::cout << i << "\tINF" << std::endl;
+//   }
+// }
+
--- main (11).cpp
+++ main (11).cpp
@@ -1,163 +1,57 @@
-/* Tarea individual: Act-Integradora-1 Conceptos básicos y algoritmos fundamentales
-Arturo Sánchez Rodríguez | A01275427
-Grant Keegan | A01700753
-28 de marzo del 2023
-Programación de estructuras de datos y algoritmos fundamentales (Gpo 850) */
+/** 
+* Compilacion para debug:  
+*    g++ -std=c++17 -g -o main *.cpp 
+* Ejecucion con valgrind:
+*    nix-env -iA nixpkgs.valgrind
+*    valgrind --leak-check=full ./main
+*
+* Compilacion para ejecucion:  
+*  g++ -std=c++17 -Wall -O3 -o main *.cpp
+*    
+* Ejecucion:
+*    ./main
 
-//En este código se están empleando los métodos de ordenamiento Insertion Sort y Bubble Sort.
+*
+* ------------------------------------
+* Equipo 2:
+* Iker Casillas  | A01641047
+* César Simental | A01641385
+* Arturo Sánchez | A01275427
+* */
 
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
-#include "class.h"
-using namespace std;
+#include <chrono>
+#include <fstream> // ayuda en lectura y escritura de archivo
+#include <sstream> // facilita separar el string
+#include <stdlib.h> //exit
+#include "MaxHeap.h"
+#include "Incidencia.h"
+#include "Graph.h"
 
-Bitacora::Bitacora(){
-} // Crea la instancia de la clase 'Bitacora'.
+int main() { 
+  int maxCapacity = 13371;
+  MaxHeap<std::pair<int, std::string>> HeapIps(maxCapacity); // Heap con informacion de ip
+  
+  std::ifstream bitacora("bitacoraGrafos.txt"); // archivo a leer
+  Graph g1(bitacora);
 
-void Bitacora::leer(){
-    string linea;
-    ifstream archivo("bitacora-1.txt");
-    if(archivo.is_open()){
-        while(getline(archivo, linea)){
-            registros.push_back(linea);
-            fechas.push_back(linea.substr(0,14));
-        }
-    }
-    archivo.close();
-};
+  
+  auto adjList = g1.getAdjList();
 
-// Este algoritmo es para el ordenamiento de Bubble Sort.
-void Bitacora::ordenar_efectivo(){
-    int comparaciones = 0; // Para contear las comparaciones e imprimirlos.
-    int swaps = 0; // Para contear los swaps e imprimirlos.
-    for(int i = 1; i < registros.size(); i++){
-        for(int j = i; j > 0; j--){
-            comparaciones++;
-            if(fechas[j] < fechas[j-1]){
-                swaps++;
-                string temp1 = fechas[j];
-                string temp2 = registros[j];
-                fechas[j] = fechas[j-1];
-                registros[j] = registros[j-1];
-                fechas[j-1] = temp1;
-                registros[j-1] = temp2;
-            }
-            else
-                break;
-        }
-    }
-    cout << "Algoritmo efectivo." << endl;
-    cout << "Comparaciones realizadas: " << comparaciones << endl;
-    cout << "Swaps realizados: " << swaps << endl;
-};
+  
+  
+  
+  std::cout << "Grafo construido" << std::endl;
 
-// Este algortimo es para el ordenamiento de Insertion Sort.
-void Bitacora::ordenar_poco_efectivo(){
-    int comparaciones = 0; // Para contear las comparaciones e imprimirlos.
-    int swaps = 0; // Para contear los swaps e imprimirlos.
-    for(int i = 0; i < registros.size()-1; i++){
-        for(int j = 0; j < registros.size()-i-1; j++){
-            comparaciones++;
-            if(fechas[j] > fechas[j+1]){
-                swaps++;
-                string temp1 = fechas[j];
-                string temp2 = registros[j];
-                fechas[j] = fechas[j+1];
-                registros[j] = registros[j+1];
-                fechas[j+1] = temp1;
-                registros[j+1] = temp2;
-            }
-        }
-    }
-    cout << "Algoritmo poco efectivo." << endl;
-    cout << "Comparaciones realizadas: " << comparaciones << endl;
-    cout << "Swaps realizados: " << swaps << endl;
-};
+  //g1.print();
+  
+  
 
-// Este metodo de la clase es para encontrar las fechas seleccionadas en el archivo.
-void Bitacora::verificar_fechas(){
-    int encontrado1 = 0;
-    int encontrado2 = 0;
-    for(int i = 0; i < registros.size(); i++){
-        if(fechas[i] == inicio){
-            encontrado1 = 1;
-        }
-        if(fechas[i] == fin){
-            encontrado2 = 1;
-        }
-    }
-    // Las siguientes lineas son por si el usuario introduce algo que no es una fecha debida.
-    if(encontrado1 == 0){
-        cout << "La fecha de inicio no se encuentra en la bitacora." << endl;
-    }
-    if(encontrado2 == 0){
-        cout << "La fecha de fin no se encuentra en la bitacora." << endl;
-    }
-};
+  // Incidencia prueba;
 
-// Este metodo es para la busqueda de fechas con los parametros que ingrese el usuario.
-void Bitacora::buscar_fechas(){
-    string inicio, fin;
-    cout << "Introduzca la fecha de inicio (mes, dia, hora, minutos, segundos): ";
-    cin >> inicio;
-    cout << "Introduzca la fecha de fin (mes, dia, hora, minutos, segundos): ";
-    cin >> fin;
-    this->inicio = inicio;
-    this->fin = fin;
-    verificar_fechas();
-    if(inicio == fin){
-        cout << "Ambas fechas son iguales. No hay informacion." << endl;
-    }
-    else{
-        int encontrado1 = 0;
-        int encontrado2 = 0;
-        int pos1 = 0;
-        int pos2 = 0;
-        for(int i = 0; i < registros.size(); i++){
-            if(fechas[i] == inicio && encontrado1 == 0){
-                encontrado1 = 1;
-                pos1 = i;
-            }
-            if(fechas[i] == fin && encontrado2 == 0){
-                encontrado2 = 1;
-                pos2 = i;
-            }
-        }
-        cout << "Los registros encontrados son:" << endl;
-        for(int i = pos1; i <= pos2; i++){
-            cout << registros[i] << endl;
-        }
-    }
-    cout << "Numero de ordenamientos: " << registros.size() << endl;
-};
+  // prueba.setIpValue("36.29.138.97", 0);
 
-// Al final esta funcion debe de imprimir los resultados de la bitacora en un archivo .txt
-void Bitacora::imprimir(){
-    for(int i = 0; i < registros.size(); i++){
-        cout << registros[i] << endl;
-    }
-};
+  // std::cout << prueba.getIpOrigenValue();
 
-//Lo que hace el siguiente código es escribir un archivo .txt de los registros que fueron almacenados en el vector.
-//Para eso creamos el archivo de texto llamado bitacora-2.txt asi analizando el registro de datos con un ciclo for y despues imprimiendo los registros en el nuevo archivo. 
-void Bitacora::imprimirN(){
-    ofstream archivo;
-    archivo.open("bitacora-2.txt");
-    for(int i = 0; i < registros.size(); i++){
-        archivo << registros[i] << endl;
-    }
-    archivo.close();
-};
-
-int main(){
-    Bitacora b;
-    b.leer();
-    b.ordenar_efectivo();
-    b.ordenar_poco_efectivo();
-    b.buscar_fechas();
-    b.imprimir();
-    b.imprimirN();
-    return 0;
-};
+  return 0; 
+}
